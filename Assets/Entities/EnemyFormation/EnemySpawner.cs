b9d47e25f8ed74adc8f1ca7e97b36cb830e1db47
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2016-11-10 19:09:06 +0530

    Learning Gizmos & movement of all enemies

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -4,15 +4,48 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemyPrefab; 
+	public float width = 15f; 
+	public float height = 10f; 
+	public float speed = 5f; 
+	public bool rightTouched = false; 
+	public float padding = 15.0f; 
+
+	public float xxxMax = 5f;
+	public float xxxMin = -9f; 
 
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity) as GameObject; 
-		enemy.transform.parent = this.transform; 
+		float distance = Camera.main.transform.position.z - transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+		xxxMax = rightBoundary.x - padding; 
+		xxxMin = leftBoundary.x + padding; 
+
+		foreach(Transform child in transform)
+		{
+			GameObject enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity) as GameObject; 
+			enemy.transform.parent = child;
+		}
 	}
-	
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(new Vector3(0,0,0), new Vector3(width, height));
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-	
+		 
+		if(transform.position.x < xxxMax && rightTouched){
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0); 
+			rightTouched = true; 
+		}else if(transform.position.x > xxxMin){
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0); 
+			rightTouched = false; 
+		}
+
+
 	}
 }
---
+++ Assets/Entities/EnemyFormation/Position.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y), 1); 
+	}
+
+}
